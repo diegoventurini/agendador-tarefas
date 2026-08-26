cdf89f4290efe518e8fc5142ba5f4a48a7da7387
diegoventurini <diegoventurini27@gmail.com>
2026-08-26 09:56:49 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/venturini/agendador_tarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.venturini.agendador_tarefas.infrastructure.security;
+
+import com.venturini.agendador_tarefas.business.dto.UsuarioDTO;
+import com.venturini.agendador_tarefas.infrastructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UsuarioClient usuarioClient;
+
+    public UserDetails loadUserByUserName(String email, String token) {
+
+        UsuarioDTO usuarioDTO = usuarioClient.buscarUsuarioPorEmail(email, token);
+
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build(); // Constrói o objeto UserDetails
+    }
+}
