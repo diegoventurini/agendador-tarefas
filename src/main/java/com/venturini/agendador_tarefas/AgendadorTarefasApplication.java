f7f236ebfa29a288146e4d9c1249181951226860
diegoventurini <diegoventurini27@gmail.com>
2026-09-03 09:02:30 -0300

    tratamento de exceções

--- src/main/java/com/venturini/agendador_tarefas/AgendadorTarefasApplication.java
+++ src/main/java/com/venturini/agendador_tarefas/AgendadorTarefasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients // Habilita o FeignClients no MAIN
+@EnableFeignClients // Habilitar o FeignClients no MAIN
 public class AgendadorTarefasApplication {
 
 	public static void main(String[] args) {
